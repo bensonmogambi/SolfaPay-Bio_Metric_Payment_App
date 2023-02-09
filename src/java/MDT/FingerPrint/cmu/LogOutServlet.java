68c2f77d1d09c34f498b1b7af3d40326318f72a2
Software Engineer <99333985+bensonmogambi@users.noreply.github.com>
2023-02-09 15:48:45 +0300

    log Out

---
+++ src/java/MDT/FingerPrint/cmu/LogOutServlet.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MDT.FingerPrint.cmu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author arjunmehrotra
+ */
+@WebServlet(name = "LogOutServlet", urlPatterns = {"/LogOutServlet"})
+public class LogOutServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         HttpSession session = request.getSession();
+        if(session != null)
+        {
+            try
+            {
+                response.setHeader("Cache-Control","no-cache"); //Forces caches to obtain a new copy of the page from the origin server
+                response.setHeader("Cache-Control","no-store"); //Directs caches not to store the page under any circumstance
+                response.setDateHeader("Expires", 0); //Causes the proxy cache to see the page as "stale"
+                response.setHeader("Pragma","no-cache"); //HTTP 1.0 backward compatibility
+                session.setAttribute("customer",null);
+                session.invalidate();
+                response.sendRedirect("index.jsp");
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+                System.out.println(e);
+            }
+
+        }
+        else
+        {
+
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
