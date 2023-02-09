52ac2ba451a1e248f38c6f5bf324663a62bff5c8
Software Engineer <99333985+bensonmogambi@users.noreply.github.com>
2023-02-09 16:04:56 +0300

    transactions

---
+++ src/java/MDT/FingerPrint/cmu/Transaction.java
@@ -0,0 +1,42 @@
+package MDT.FingerPrint.cmu;
+
+
+
+public class Transaction {
+	
+	private String transactionNumber;
+	private String creditCardNumber;
+	private String vendor;
+	private String transactionDate;
+	private String transactionAmount;
+	public String getTransactionNumber() {
+		return transactionNumber;
+	}
+	public void setTransactionNumber(String transactionNumber) {
+		this.transactionNumber = transactionNumber;
+	}
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+	public String getVendor() {
+		return vendor;
+	}
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+	public String getTransactionDate() {
+		return transactionDate;
+	}
+	public void setTransactionDate(String transactionDate) {
+		this.transactionDate = transactionDate;
+	}
+	public String getTransactionAmount() {
+		return transactionAmount;
+	}
+	public void setTransactionAmount(String transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
+}
